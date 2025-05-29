79a92979a0e944c02ab3f7e2ff1a1fa75c7a8588
charefanis <anicharef74@gmail.com>
2025-05-29 11:15:01 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// Scroll to the top when the button is clicked
-scrollToTopBtn.addEventListener('click', function() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-function toggleDarkMode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode")
-}
